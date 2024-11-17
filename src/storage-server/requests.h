0e92ba3849ff6df031853a563282c5ac548dcd10
gautambhetanabhotla <gautam.bhetanabhotla@students.iiit.ac.in>
2024-11-18 00:34:57 +0530

    changing format, not complete

--- src/storage-server/requests.h
+++ src/storage-server/requests.h
@@ -9,12 +9,13 @@
 
 void* handle_client(void* arg);
 void ss_read(int fd, char* vpath);
-void ss_write(int fd, char* vpath, char* mtime, int contentLength);
+void ss_write(int fd, char* vpath, int contentLength);
 struct file* ss_create(int fd, char* vpath, char* mtime);
 void ss_delete(int fd, char* vpath);
 void ss_stream(int fd, char* vpath);
-void ss_copy(int fd, char* vpath);
+void ss_copy(int fd, char* vpath, char* vpath2);
 void ss_info(int fd, char* vpath);
 void ss_update_mtime(int fd, char* vpath, char* mtime);
+void ns_synchronize(int fd, char* vpath);
 
 #endif
