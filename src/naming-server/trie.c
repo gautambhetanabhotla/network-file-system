66080255b37cb1b205f5fadab0860b56c9cb1ec9
MrudaniPimpalkhare <mrudani@fairshare.tech>
2024-12-02 14:53:37 +0530

    deletuh

--- src/naming-server/trie.c
+++ src/naming-server/trie.c
@@ -14,7 +14,77 @@ TrieNode *create_trie_node()
     return node;
 }
 
-FileEntry* insert_path(const char *path, int *storage_server_ids, int num_chosen, TrieNode *root)
+FileEntry* insert_path_forc(const char *path, int *storage_server_ids, int num_chosen, TrieNode *root)
+{
+    // need to make sure that when a file/directory is added, the parent is also a directory
+    TrieNode *current = root;
+    if(strlen(path)==1){
+        fprintf(stderr, "only one character, is it root?\n");
+        if(path[0]=='/'){
+            fprintf(stderr, "omg its root you go girl!\n");
+            return root->file_entry;
+        }
+    }else{
+        if(path[0]!='/'){
+            fprintf(stderr, "path does not start with /\n");
+            return NULL;
+        }
+    }
+    for (int i = 1; path[i]; i++)
+    {
+        if(current->file_entry && (current->file_entry->is_folder==0))
+        {
+            if(i==strlen(path)-2){
+                fprintf(stderr, "parent is not a folder\n");
+                return NULL;
+            }
+        }
+        unsigned char index = (unsigned char)path[i];
+        // need to check if the parent is a directory
+        if (!current->children[index]){
+            if(index == '/'){
+                if(i== (strlen(path)-1))
+                {
+                    current->children[index] = create_trie_node();
+                }else{
+                    return NULL;
+                }
+                
+            }else{
+                current->children[index] = create_trie_node();
+            }
+        }
+        current = current->children[index];
+    }
+
+    if (current->file_entry)
+    {
+        // File already exists
+        return current->file_entry;
+    }
+    if (!current->file_entry)
+    {
+        current->file_entry = (FileEntry *)malloc(sizeof(FileEntry));
+        strcpy(current->file_entry->filename, path);
+        fprintf(stderr, "path: %s\n", current->file_entry->filename);
+
+        for (int i = 0; i < num_chosen; i++)
+        {
+            fprintf(stderr, "storage server id: %d\n", storage_server_ids[i]);
+            current->file_entry->ss_ids[i] = storage_server_ids[i];
+            storage_servers[storage_server_ids[i]].file_count++;
+            fprintf(stderr, "file count: %d\n", storage_servers[storage_server_ids[i]].file_count);
+        }
+        for(int i = num_chosen; i<3; i++){
+            current->file_entry->ss_ids[i] = -1;
+        }
+    }
+
+    return current->file_entry;
+}
+
+
+FileEntry* insert_path_forss(const char *path, int *storage_server_ids, int num_chosen, TrieNode *root)
 {
     // need to make sure that when a file/directory is added, the parent is also a directory
     TrieNode *current = root;
