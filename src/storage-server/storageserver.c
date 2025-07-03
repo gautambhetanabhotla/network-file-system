ca59cd5415f48dacbfaf963685e583bba0272b08
gautambhetanabhotla <gautam.bhetanabhotla@students.iiit.ac.in>
2025-07-03 21:23:21 +0530

    Change signature of storage server side functions

--- src/storage-server/storageserver.c
+++ src/storage-server/storageserver.c
@@ -30,32 +30,32 @@ void* handle_client(void* arg) {
     fprintf(stderr, "Client arrived!\n");
     int client_sockfd = *(int*)arg;
     request_header req;
-    recv(client_sockfd, &req, sizeof(req), 0);
+    recv(client_sockfd, &req, sizeof(req), MSG_WAITALL);
     request_to_string(&req);
     switch(req.type) {
         case READ:
-            ss_read(client_sockfd, req.paths[0], req.id, req.paths[1], req.contentLength);
+            ss_read(&req, client_sockfd);
             break;
         case WRITE:
-            ss_write(client_sockfd, req.paths[0], req.contentLength, req.id, req.paths[1], req.contentLength);
+            ss_write(&req, client_sockfd);
             break;
         case CREATE:
-            ss_create(client_sockfd, req.paths[0], UNIX_START_TIME, req.id, req.contentLength, req.paths[1], req.contentLength);
+            ss_create(&req, client_sockfd);
             break;
         case DELETE:
-            ss_delete(client_sockfd, req.paths[0], req.id, req.paths[1], req.contentLength);
+            ss_delete(&req, client_sockfd);
             break;
         case STREAM:
-            ss_stream(client_sockfd, req.paths[0], req.id, req.paths[1], req.contentLength);
+            ss_read(&req, client_sockfd);
             break;
         case COPY:
-            ss_copy(client_sockfd, req.paths[0], req.id, req.paths[1], req.contentLength);
+            ss_copy(&req, client_sockfd);
             break;
         case INFO:
-            ss_info(client_sockfd, req.paths[0], req.id, req.paths[1], req.contentLength);
+            ss_info(&req, client_sockfd);
             break;
         default:
-            send(client_sockfd, "tf u sending brother??????????\n", strlen("tf u sending brother??????????\n"), 0);
+            respond(nm_sockfd, client_sockfd, E_WRONG_SS, req.id, 0, NULL, 0);
             break;
     }
     close(client_sockfd);
@@ -63,15 +63,15 @@ void* handle_client(void* arg) {
     return NULL;
 }
 
-void ss_read(int fd, char* vpath, int requestID, char* tbf, int rcl) {
-    struct file* f = get_file(vpath);
+void ss_read(request_header* req, int client_sockfd) {
+    struct file* f = get_file(req->paths[0]);
     if(!f) {
-        respond(nm_sockfd, fd, E_WRONG_SS, requestID, 0, NULL, 0);
+        respond(nm_sockfd, client_sockfd, E_WRONG_SS, req->id, 0, NULL, 0);
         return;
     }
     FILE* F = fopen(f->rpath, "r");
     if(!F) {
-        respond(nm_sockfd, fd, E_FAULTY_SS, requestID, 0, NULL, 0);
+        respond(nm_sockfd, client_sockfd, E_FAULTY_SS, req->id, 0, NULL, 0);
         return;
     }
     fseek(F, 0, SEEK_END);
@@ -84,10 +84,10 @@ void ss_read(int fd, char* vpath, int requestID, char* tbf, int rcl) {
     if(f->readers == 1) sem_wait(&f->writelock);
     sem_post(&f->lock);
     sem_post(&f->serviceQueue);
-    respond(-1, fd, ACK, requestID, file_size, NULL, 0);
+    respond(-1, client_sockfd, ACK, req->id, file_size, NULL, 0);
     while(!feof(F)) {
         n = fread(buf, 1, 8192, F);
-        if(n > 0) send(fd, buf, n, 0);
+        if(n > 0) send(client_sockfd, buf, n, 0);
     }
     sem_wait(&f->lock);
     f->readers--;
@@ -95,79 +95,51 @@ void ss_read(int fd, char* vpath, int requestID, char* tbf, int rcl) {
     sem_post(&f->lock);
 }
 
-struct arg1 {
-    int port;
-    char* ip;
-    char* vpath;
-    struct file* f;
-};
+// struct arg1 {
+//     int port;
+//     char* ip;
+//     char* vpath;
+//     struct file* f;
+// };
 
-void ss_write(int fd, char* vpath, int contentLength, int requestID, char* tbf, int rcl) {
-    char* tbf_old = tbf;
-    contentLength = rcl;
-    // contentLength -= (strlen(vpath) + 1);
+void ss_write(request_header* req, int client_sockfd) {
+
+    struct file* f = get_file(req->paths[0]);
+    if(!f) {
+        respond(nm_sockfd, client_sockfd, E_WRONG_SS, req->id, 0, NULL, 0);
+        return;
+    }
     
-    // Obtain IPs and ports of other Storage servers
-    char *ip1, *ip2, *port1, *port2, *saveptr;
-    ip1 = __strtok_r(tbf, "\n", &saveptr);
-    port1 = __strtok_r(NULL, "\n", &saveptr);
-    ip2 = __strtok_r(NULL, "\n", &saveptr);
-    port2 = __strtok_r(NULL, "\n", &saveptr);
-    contentLength -= (saveptr - tbf_old);
-    int p1 = atoi(port1), p2 = atoi(port2);
-
-    struct file* f = get_file(vpath);
-    if(!f) {
-        respond(nm_sockfd, fd, E_WRONG_SS, requestID, 0, NULL, 0);
-        return;
-    }
-    if(!f) {
-        respond(nm_sockfd, fd, E_FAULTY_SS, requestID, 0, NULL, 0);
-        return;
-    }
-
     // Open and write to the file
     FILE* F = fopen(f->rpath, "w");
-    char buf[8193]; int n = 0;
+    if(!F) {
+        respond(nm_sockfd, client_sockfd, E_FAULTY_SS, req->id, 0, NULL, 0);
+        return;
+    }
+    char buf[8193]; int bytes_written = 0;
     sem_wait(&f->serviceQueue);
     sem_wait(&f->writelock);
     sem_post(&f->serviceQueue);
-    respond(-1, fd, ACK, requestID, 0, NULL, 0);
-    n += fwrite(tbf + rcl - contentLength, 1, rcl, F);
-    while(n < contentLength) {
-        int k = recv(fd, buf, 8192, 0);
+    respond(-1, client_sockfd, ACK, req->id, 0, NULL, 0);
+    while(bytes_written < req->contentLength) {
+        int k = recv(client_sockfd, buf, 8192, 0);
         if(k == 0) break;
         fwrite(buf, 1, k, F);
-        n += k;
+        bytes_written += k;
     }
     fclose(F);
     sem_post(&f->writelock);
-    if(n < contentLength) respond(nm_sockfd, fd, E_INCOMPLETE_WRITE, requestID, 0, NULL, 0);
-    else respond(nm_sockfd, fd, SUCCESS, requestID, 0, NULL, 0);
+    if(bytes_written < req->contentLength) respond(nm_sockfd, client_sockfd, E_INCOMPLETE_WRITE, req->id, 0, NULL, 0);
+    else respond(nm_sockfd, client_sockfd, SUCCESS, req->id, 0, NULL, 0);
 
-    // Copy the file to other Storage servers
-    pthread_t t1, t2;
-    struct arg1* arg = (struct arg1*)malloc(sizeof(struct arg1));
-    arg->port = p1;
-    arg->ip = strdup(ip1);
-    arg->vpath = strdup(vpath);
-    arg->f = f;
-    struct arg1* arg2 = (struct arg1*)malloc(sizeof(struct arg1));
-    arg2->port = p2;
-    arg2->ip = strdup(ip2);
-    arg2->vpath = strdup(vpath);
-    arg2->f = f;
-    // pthread_create(&t1, NULL, send_file, arg);
-    // pthread_create(&t2, NULL, send_file, arg2);
+    // TODO: Copy the file to other storage servers
 }
 
-struct file* ss_create(int fd, char* vpath, char* mtime, int requestID, int contentLength, char* tbf, int rcl) {
-    contentLength -= (strlen(vpath) + 1);
-    tbf[contentLength] = '\0';
-    mtime = tbf + 3;
+struct file* ss_create(request_header* req, int client_sockfd) {
+    char* vpath = req->paths[0];
     struct file* g = NULL;
     if((g = get_file(vpath))) {
-        respond(nm_sockfd, -1, E_FILE_ALREADY_EXISTS, requestID, 0, NULL, 0);
+        respond(nm_sockfd, -1, E_FILE_ALREADY_EXISTS, req->id, 0, NULL, 0);
         return g;
     }
     char rpath[MAXPATHLENGTH + 1];
@@ -183,115 +155,61 @@ struct file* ss_create(int fd, char* vpath, char* mtime, int requestID, int cont
     // fclose(p);
     FILE* ff = fopen(rpath, "w");
     fclose(ff);
+    char mtime[20];
+    time_t now = time(NULL);
+    struct tm* local_time = localtime(&now);
+    strftime(mtime, sizeof(mtime), "%Y-%m-%dT%H:%M:%S", local_time);
     struct file* f = add_file_entry(vpath, rpath, mtime, true);
-    if(f == NULL) respond(nm_sockfd, fd, E_FAULTY_SS, requestID, 0, NULL, 0);
-    else respond(nm_sockfd, fd, SUCCESS, requestID, 0, NULL, 0);
+    if(f == NULL) respond(nm_sockfd, client_sockfd, E_FAULTY_SS, req->id, 0, NULL, 0);
+    else respond(nm_sockfd, client_sockfd, SUCCESS, req->id, 0, NULL, 0);
     return f;
 }
 
-void ss_delete(int fd, char* vpath, int requestID, char* tbf, int rcl) {
-    struct file* f = get_file(vpath);
-    if(!f) respond(nm_sockfd, fd, E_FILE_DOESNT_EXIST, requestID, 0, NULL, 0);
+void ss_delete(request_header* req, int client_sockfd) {
+    struct file* f = get_file(req->paths[0]);
+    if(!f) respond(nm_sockfd, client_sockfd, E_FILE_DOESNT_EXIST, req->id, 0, NULL, 0);
     else if(remove(f->rpath)) {
-        respond(nm_sockfd, -1, E_FAULTY_SS, requestID, 0, NULL, 0);
+        respond(nm_sockfd, client_sockfd, E_FAULTY_SS, req->id, 0, NULL, 0);
     }
     else {
-        remove_file_entry(vpath);
-        respond(nm_sockfd, -1, SUCCESS, requestID, 0, NULL, 0);
+        remove_file_entry(req->paths[0]);
+        respond(nm_sockfd, client_sockfd, SUCCESS, req->id, 0, NULL, 0);
     }
 }
 
-void ss_stream(int fd, char* vpath, int requestID, char* tbf, int rcl) {
-    struct file* f = get_file(vpath);
-    if(!f) {
-        respond(nm_sockfd, fd, E_WRONG_SS, requestID, 0, NULL, 0);
-        return;
-    }
-    FILE* F = fopen(f->rpath, "rb");
-    if(!F) {
-        respond(nm_sockfd, fd, E_FAULTY_SS, requestID, 0, NULL, 0);
-        return;
-    }
-    fseek(F, 0, SEEK_END);
-    long file_size = ftell(F);
-    rewind(F);
-    char buf[8193]; int n = 0;
-    sem_wait(&f->serviceQueue);
-    sem_wait(&f->lock);
-    f->readers++;
-    if(f->readers == 1) sem_wait(&f->writelock);
-    sem_post(&f->lock);
-    sem_post(&f->serviceQueue);
-    respond(-1, fd, file_size, requestID, file_size, NULL, 0);
-    while(!feof(F)) {
-        n = fread(buf, 1, 8192, F);
-        if(n > 0) send(fd, buf, n, 0);
-    }
-    sem_wait(&f->lock);
-    f->readers--;
-    if(f->readers == 0) sem_post(&f->writelock);
-    sem_post(&f->lock);
+void ss_copy(request_header* req, int client_sockfd) {
+    // struct file* sourcefile = get_file(req->paths[0]);
+    // char* dstpath = req->paths[1];
+
+    // if(!sourcefile) {
+    //     respond(nm_sockfd, client_sockfd, E_FILE_DOESNT_EXIST, req->id, 0, NULL, 0);
+    //     return;
+    // }
+    // FILE* F = fopen(sourcefile->rpath, "r");
+    // fseek(F, 0, SEEK_END);
+    // long long file_size = ftell(F);
+    // rewind(F);
+    // char* paths[] = {dstpath, NULL};
+    // request(-1, destfd, WRITE, file_size, paths, NULL, NULL);
+    // char buf[8193]; int n = 0;
+    // send(destfd, dstpath, strlen(dstpath), 0);
+    // while(!feof(F)) {
+    //     n = fread(buf, 1, 8192, F);
+    //     if(n > 0) send(destfd, buf, n, 0);
+    // }
+    // fclose(F);
+    // respond(nm_sockfd, fd, SUCCESS, requestID, 0, NULL, 0);
 }
 
-void ss_copy(int fd, char* srcpath, int requestID, char* tbf, int rcl) {
-    struct file* f = get_file(srcpath);
-    char *fp = tbf, *fpp = fp;
-    while(*fp != '\n') fp++;
-    *fp = '\0';
-    fp++;
-    char* dstpath = (char*) malloc(4097 * sizeof(char));
-    strcpy(dstpath, fpp);
-    dstpath[strlen(dstpath)] = '\n';
-    fpp = fp;
-    while(*fp != '\n') fp++;
-    *fp = '\0';
-    fp++;
-    char* IP = (char*) malloc(100 * sizeof(char));
-    strcpy(IP, fpp);
-    fpp = fp;
-    while(*fp != '\n') fp++;
-    *fp = '\0';
-    fp++;
-    int port = atoi(fpp);
-
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr(IP);
-    int destfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(connect(destfd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
-        respond(nm_sockfd, fd, E_CONN_REFUSED, requestID, 0, NULL, 0);
-    }
-    free(IP);
-    if(!f) {
-        respond(nm_sockfd, fd, E_FILE_DOESNT_EXIST, requestID, 0, NULL, 0);
-        return;
-    }
-    FILE* F = fopen(f->rpath, "r");
-    fseek(F, 0, SEEK_END);
-    long long file_size = ftell(F);
-    rewind(F);
-    char* paths[] = {dstpath, NULL};
-    request(-1, destfd, WRITE, file_size, paths, NULL, NULL);
-    char buf[8193]; int n = 0;
-    send(destfd, dstpath, strlen(dstpath), 0);
-    while(!feof(F)) {
-        n = fread(buf, 1, 8192, F);
-        if(n > 0) send(destfd, buf, n, 0);
-    }
-    fclose(F);
-    respond(nm_sockfd, fd, SUCCESS, requestID, 0, NULL, 0);
-}
-
-void ss_info(int fd, char* vpath, int requestID, char* tbf, int rcl) {
-    struct file* f = get_file(vpath);
+void ss_info(request_header* req, int client_sockfd) {
+    struct file* f = get_file(req->paths[0]);
     if(f == NULL) {
-        send(fd, "File not found\n", strlen("File not found\n"), 0);
+        respond(nm_sockfd, client_sockfd, E_WRONG_SS, req->id, 0, NULL, 0);
         return;
     }
     FILE* F = fopen(f->rpath, "r");
     if(F == NULL) {
-        send(fd, "File not found\n", strlen("File not found\n"), 0);
+        respond(nm_sockfd, client_sockfd, E_FAULTY_SS, req->id, 0, NULL, 0);
         return;
     }
 
@@ -313,8 +231,8 @@ void ss_info(int fd, char* vpath, int requestID, char* tbf, int rcl) {
     // Send the file info to the client
     char buffer[1024];
     sprintf(buffer, "File size: %ld bytes\nNumber of lines: %d\nLast modified: %s\n", file_size, line_count, f->mtime);
-    respond(nm_sockfd, fd, SUCCESS, requestID, (long)strlen(buffer), NULL, 0);
-    send(fd, buffer, strlen(buffer), 0);
+    respond(nm_sockfd, client_sockfd, SUCCESS, req->id, (long)strlen(buffer), NULL, 0);
+    send(client_sockfd, buffer, strlen(buffer), 0);
 
     fclose(F);
 }
